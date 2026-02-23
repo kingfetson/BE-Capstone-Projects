d7901096d463ead76dcfcc0299fdcfb5e17927db
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-02-23 11:46:57 +0300

    Added API routes

--- Recipe_Management_API/Recipe_Management_API/urls.py
+++ Recipe_Management_API/Recipe_Management_API/urls.py
@@ -21,6 +21,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
@@ -30,4 +37,8 @@ urlpatterns = [
 
     # App APIs
     path("api/", include("recipes.urls")),
+
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
